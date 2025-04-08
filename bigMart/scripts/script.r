67a19933eae0defdb5b7541e7024478569873b3a
chadUSFBulls <chadbloom@usf.edu>
2025-04-07 21:44:00 -0400

    started script

---
+++ bigMart/scripts/script.r
@@ -0,0 +1,6 @@
+
+# import data 
+library(readxl)
+raw_data <- read_excel("C:\\Users\\cbloom\\Documents\\dev\\huntersGreen\\huntersGreen\\bigMart\\data\\BigMartSales-2.xlsx")
+View(raw_data)
+str(raw_data)
