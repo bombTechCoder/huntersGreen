2b4240d3efd07794e9102a25c08ae2e5f98988fd
chadUSFBulls <chadbloom@usf.edu>
2025-04-07 22:17:15 -0400

    progress

--- bigMart/scripts/script.r
+++ bigMart/scripts/script.r
@@ -2,5 +2,14 @@
 # import data 
 library(readxl)
 raw_data <- read_excel("C:\\Users\\cbloom\\Documents\\dev\\huntersGreen\\huntersGreen\\bigMart\\data\\BigMartSales-2.xlsx")
-View(raw_data)
+
+# View(raw_data)
+str(raw_data)
+
+raw_data$item_fat_content <- factor(raw_data$item_fat_content)
+raw_data$item_type <- factor(raw_data$item_fat_content)
+raw_data$outlet_size <- factor(raw_data$outlet_size)
+raw_data$city_type <- factor(raw_data$city_type)
+raw_data$outlet_type <- factor(raw_data$outlet_type)
+
 str(raw_data)
