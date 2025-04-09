f7ba3ecb777b4d6cdf3b30c171125fab1ab31ad2
chadUSFBulls <chadbloom@usf.edu>
2025-04-08 20:33:25 -0400

    ilhg;ioju

--- bigMart/scripts/script.r
+++ bigMart/scripts/script.r
@@ -12,4 +12,16 @@ raw_data$outlet_size <- factor(raw_data$outlet_size)
 raw_data$city_type <- factor(raw_data$city_type)
 raw_data$outlet_type <- factor(raw_data$outlet_type)
 
+View(raw_data)
 str(raw_data)
+hist(raw_data$item_sales)
+
+summary(raw_data)
+
+cor(raw_data$item_sales, raw_data$item_visibility)
+cor(raw_data$item_sales, raw_data$item_mrp)
+# cor(raw_data$item_sales, raw_data$outlet_type)
+
+library(corrplot)
+corrplot(raw_data, method="circle")
+library(ggplot2)
