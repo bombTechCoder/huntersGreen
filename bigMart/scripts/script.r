df28a251e6cf5b12ced288d9b91fb5f7ef58bd75
chadUSFBulls <chadbloom@usf.edu>
2025-04-09 21:40:37 -0400

    work

--- bigMart/scripts/script.r
+++ bigMart/scripts/script.r
@@ -1,27 +1,37 @@
-
 # import data 
 library(readxl)
-raw_data <- read_excel("C:\\Users\\cbloom\\Documents\\dev\\huntersGreen\\huntersGreen\\bigMart\\data\\BigMartSales-2.xlsx")
+library(dplyr)
+library(plm)
+d <- read_excel("C:\\Users\\cbloom\\Documents\\dev\\huntersGreen\\huntersGreen\\bigMart\\data\\BigMartSales-2.xlsx")
 
-# View(raw_data)
-str(raw_data)
+# View(d)
+str(d)
 
-raw_data$item_fat_content <- factor(raw_data$item_fat_content)
-raw_data$item_type <- factor(raw_data$item_fat_content)
-raw_data$outlet_size <- factor(raw_data$outlet_size)
-raw_data$city_type <- factor(raw_data$city_type)
-raw_data$outlet_type <- factor(raw_data$outlet_type)
+# d$item_fat_content <- factor(d$item_fat_content)
+d$item_type <- factor(d$item_fat_content)
+d$outlet_size <- factor(d$outlet_size)
+d$city_type <- factor(d$city_type)
+d$outlet_type <- factor(d$outlet_type)
+d$outlet_age <- 2025 - d$outlet_year
 
-View(raw_data)
-str(raw_data)
-hist(raw_data$item_sales)
+d$item_fat_content <- recode_factor(d$item_fat_content, "low fat"="Low Fat")
 
-summary(raw_data)
+View(d)
+str(d)
+hist(log(d$item_sales))
 
-cor(raw_data$item_sales, raw_data$item_visibility)
-cor(raw_data$item_sales, raw_data$item_mrp)
-# cor(raw_data$item_sales, raw_data$outlet_type)
+summary(d)
 
-library(corrplot)
-corrplot(raw_data, method="circle")
-library(ggplot2)
+cor(d$item_sales, d$item_visibility)
+cor(d$item_sales, d$item_mrp)
+
+otm1_ols <- lm(log(item_sales) ~ outlet_type, data=d)
+summary(otm1_ols)
+plot(otm1_ols)
+
+otm2_glm <- glm(log(item_sales) ~ outlet_type, data=d)
+summary(otm2_glm)
+plot(otm2_glm)
+
+otm3_plm <- plm(log(item_sales) ~ outlet_type, data=d, model="random")
+summary(otm3_plm)
